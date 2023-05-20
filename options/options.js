9ce1a73eb6849cd4189d70d02bcde1f235a0ae9d
Crowdbotics <engineering@crowdbotics.com>
2023-05-20 23:20:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-cloud-dev-84940",
+  url: "https://quiet-cloud-dev-84940.botics.co",
+  api: "https://quiet-cloud-dev-84940.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
